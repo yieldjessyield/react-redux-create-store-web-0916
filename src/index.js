69b5a2512331d8e524f08156d3f9f4e45d77b401
ipc103 <iancandysemail@gmail.com>
2016-11-01 14:51:06 -0400

    add lesson

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
-const app = "I don't do much.";
-console.log(app);
+import { createStore } from 'redux';
+import counterReducer from './reducers/counterReducer';
+console.log("WHatever")
+const store = createStore(counterReducer);
+
+
+console.log( store.getState() );
+store.dispatch({type: 'INCREMENT_COUNT'})
+store.dispatch({type: 'INCREMENT_COUNT'})
+console.log( store.getState() );
+store.dispatch({type: 'DECREMENT_COUNT'})
+console.log( store.getState() );
+store.dispatch({type: 'INCREMENT_COUNT'})
+store.dispatch({type: 'INCREMENT_COUNT'})
+console.log( store.getState() );
+store.dispatch({type: 'RESET_COUNT'});
+console.log( store.getState() );
---
+++ src/reducers/counterReducer.js
@@ -0,0 +1,12 @@
+export default function counterReducer(state=0, action){
+  switch (action.type) {
+    case 'INCREMENT_COUNT':
+      return state + 1;
+    case 'DECREMENT_COUNT':
+      return state - 1;
+    case 'RESET_COUNT':
+      return 0;
+    default:
+      return state;
+  }
+}
